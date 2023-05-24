3dbbabd43bc7b6cb1e1fc457f7b25c3735ffc866
nowl <langcauonoel@gmail.com>
2023-05-24 10:21:21 +0800

    convert to v4.8

--- IpaBisikletaMo_Application/Models/IpaBisikletaMo.Designer.cs
+++ IpaBisikletaMo_Application/Models/IpaBisikletaMo.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\hp\source\repos\IpaBisikletaMo_Application\IpaBisikletaMo_Application\Models\IpaBisikletaMo.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\syste\source\repos\IpaBisikletaMo_Application\IpaBisikletaMo_Application\Models\IpaBisikletaMo.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
